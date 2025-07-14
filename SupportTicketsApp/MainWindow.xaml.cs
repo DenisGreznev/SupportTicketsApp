ac2dd2ce5a3a2fc79baf49b46c60f83f7f68b9b2
den.greznev2016@yandex.ru <den.greznev@yandex.ru>
2025-07-14 15:50:59 +0300

    Version 1.2

--- SupportTicketsApp/MainWindow.xaml.cs
+++ SupportTicketsApp/MainWindow.xaml.cs
@@ -24,5 +24,84 @@ namespace SupportTicketsApp
         {
             InitializeComponent();
         }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void rdZayavki_Click(object sender, RoutedEventArgs e)
+        {
+            // PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("Pages/ZayavkiPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdKlients_Click(object sender, RoutedEventArgs e)
+        {
+            // PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("Pages/KlientsPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdSotrudniki_Click(object sender, RoutedEventArgs e)
+        {
+            // PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("Pages/SotrudnikiPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdOtch_Click(object sender, RoutedEventArgs e)
+        {
+            // PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("Pages/OtchPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void home_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            btnMenu.IsChecked = true;
+            PagesNavigation.Navigate(new System.Uri("Pages/ZayavkiPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void home_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите выйти?", "Выход", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void buttondob_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonprofile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ SupportTicketsApp/Pages/ZayavkiPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SupportTicketsApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ZayavkiPage.xaml
+    /// </summary>
+    public partial class ZayavkiPage : Page
+    {
+        public ZayavkiPage()
+        {
+            InitializeComponent();
+        }
+
+        private void datagrid_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void datagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Filter_Changed(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
